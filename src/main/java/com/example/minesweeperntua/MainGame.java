fdf78339ae30b95714d4fa1a67be05779a19c7de
pGiad <panosgiadi@gmail.com>
2023-03-08 13:49:56 +0200

    javadoc Documentation

--- src/main/java/com/example/minesweeperntua/MainGame.java
+++ src/main/java/com/example/minesweeperntua/MainGame.java
@@ -13,56 +13,108 @@ public class MainGame {
     private int tries = 0;
     private boolean gameFinished = false;
 
+    /**
+     * Constructs a new instance of the MainGame class with the specified Minesweeper instance.
+     *
+     * @param minesweeperApp the MinesweeperApp instance. Used to initialize the `minesweeperApp` field and the `tiles`
+     *                       array with the size of the grid retrieved from `MinesweeperApp`.
+     */
     public MainGame(MinesweeperApp minesweeperApp) {
         this.minesweeperApp = minesweeperApp;
         minesweeperApp.setLoadedScenario(false);
         this.tiles = new Tile[getMinesweeperApp().getGridSize()][getMinesweeperApp().getGridSize()];
     }
 
+    /**
+     * Returns the MinesweeperApp instance associated with this game. It is widely used from other classes to access the
+     * fields of MinesweeperApp like the difficulty of the game, the number of mines, the time, if there is a super-mine
+     * etc.
+     *
+     * @return the MinesweeperApp instance
+     */
     public MinesweeperApp getMinesweeperApp() {
         return minesweeperApp;
     }
 
+    /**
+     * Returns the game board as a 2D array of Tiles. Tile class has important information about each tile like if it is
+     * opened or flagged, if it is a mine or super-mine.
+     *
+     * @return the game board
+     */
     public Tile[][] getTiles() {
         return tiles;
     }
 
+    /**
+     * Sets the game board to the specified 2D array of Tiles.
+     *
+     * @param tiles the new game board
+     */
     public void setTiles(Tile[][] tiles) {
         this.tiles = tiles;
     }
 
-    public int getNumOfOpenedTiles() {
-        return numOfOpenedTiles;
-    }
-
-    public void setNumOfOpenedTiles(int numOfOpenedTiles) {
-        this.numOfOpenedTiles = numOfOpenedTiles;
-    }
-
+    /**
+     * Returns the number of flags used in the game.
+     *
+     * @return the number of flags used
+     */
     public int getUsedFlags() {
         return usedFlags;
     }
 
+    /**
+     * Sets the number of flags used in the game.
+     *
+     * @param usedFlags the number of flags used
+     */
     public void setUsedFlags(int usedFlags) {
         this.usedFlags = usedFlags;
     }
 
+    /**
+     * Returns the number of tries made (each successful left click is a try).
+     *
+     * @return the number of tries made
+     */
     public int getTries() {
         return tries;
     }
 
-    public boolean isGameFinished() {
-        return gameFinished;
-    }
-
-    public void setGameFinished(boolean gameFinished) {
-        this.gameFinished = gameFinished;
-    }
-
+    /**
+     * Sets the number of tries made.
+     *
+     * @param tries the number of tries made
+     */
     public void setTries(int tries) {
         this.tries = tries;
     }
 
+    /**
+     * Returns whether the game is finished or not.
+     *
+     * @return {@code true} if the game is finished, {@code false} otherwise
+     */
+    public boolean isGameFinished() {
+        return gameFinished;
+    }
+
+    /**
+     * Sets the game finished state.
+     *
+     * @param gameFinished {@code true} if the game is finished, {@code false} otherwise
+     */
+    public void setGameFinished(boolean gameFinished) {
+        this.gameFinished = gameFinished;
+    }
+
+    /**
+     * Returns the number of mine neighbors for the given list of tiles.
+     *
+     * @param neighbors the list of neighboring tiles
+     * @return the number of mine neighbors for the given list of tiles
+     */
     public int getBombNeighbors(List<Tile> neighbors) {
         int bombNeighbors = 0;
 
@@ -74,6 +126,13 @@ public class MainGame {
         return bombNeighbors;
     }
 
+    /**
+     * Returns a list of neighboring tiles for the given tile and difficulty level.
+     *
+     * @param tile the tile to get neighbors for
+     * @param difficulty the difficulty level (1 for easy, 2 for medium)
+     * @return a list of neighboring tiles for the given tile and difficulty level
+     */
     public List<Tile> getNeighbors(Tile tile, int difficulty) {
         List<Tile> neighbors = new ArrayList<>();
 
@@ -109,6 +168,11 @@ public class MainGame {
         return neighbors;
     }
 
+    /**
+     * Opens the given tile.
+     *
+     * @param tile the tile to open
+     */
     public void open(Tile tile) {
         if (tile.isOpen()) {
             return;
@@ -147,6 +211,12 @@ public class MainGame {
         }
     }
 
+    /**
+     * Toggles the flag state of a given tile, unless the tile is already open or the player
+     * has already used all their flags and is trying to add another flag.
+     *
+     * @param tile The tile to toggle the flag state of.
+     */
     public void flag(Tile tile) {
         if (tile.isOpen() || (usedFlags == minesweeperApp.getBombs() && !tile.isFlag())) {
             return;
@@ -190,6 +260,16 @@ public class MainGame {
         }
     }
 
+    /**
+     * Updates the list of recent rounds in the rounds.txt file with a new entry representing the
+     * current game, containing the number of mines, the number of tries, the time taken, and whether
+     * the user won or not.
+     *
+     * @param bombs    The number of mines in the current game.
+     * @param tries    The number of tries taken in the current game.
+     * @param time     The time taken to complete the current game, in seconds.
+     * @param userWon  True if the user won the current game, false otherwise.
+     */
     public void updateRounds(int bombs, int tries, int time, boolean userWon) {
         try {
             String filename = "medialab/rounds.txt";
